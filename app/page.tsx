b893f359e8d9513d81336f42ec69b5ed2837b69e
clairejong <chj2130@columbia.edu>
2026-01-30 11:27:52 -0500

    Increased text size

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <h1>Hello World</h1>
+      <h1 style={{ fontSize: '5rem' }}>Hello World</h1>
     </main>
   );
 }
